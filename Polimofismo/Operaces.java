d893f51c6d0f7d7ff153c41befbccd597126965c
Marialuiza81 <144393629+marialuizavi81@users.noreply.github.com>
2024-12-12 15:32:57 -0300

    Add files via upload

---
+++ Polimofismo/Operaces.java
@@ -0,0 +1,14 @@
+package Polimofismo;
+
+public class Operaces {
+	
+	public void somar(double num1,double num2){
+		System.out.println(num1 + num2);
+	} 
+	public void somar( int num1,double num2) {
+		System.out.println(num1+num2);
+	}
+	public void somar(int num1, int num2, int num3) {
+		System.out.println(num1+num2+num3);
+	}
+}
---
+++ Polimofismo/calculadora.java
@@ -0,0 +1,13 @@
+package Polimofismo;
+
+public class calculadora {
+	public static void main(String[] args) {
+		
+		Operaces c1 = new Operaces();
+		
+		c1.somar(12,4);
+		c1.somar(2, 340);
+		c1.somar(87, 230, 8);
+		
+	}
+}
